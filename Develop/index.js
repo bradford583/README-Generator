b829658498a03a5a2c42bae862eea6c8647e8148
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-09-18 17:40:22 -0600

    finished questions, working on writeFile

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const util = require('util')
 const fs = require('fs');
+const Choices = require('inquirer/lib/objects/choices');
 // TODO: Create an array of questions for user input
 const questions = [
     //get users name
@@ -26,8 +27,8 @@ const questions = [
     //get repos
     {
         type:'input',
-        name:'repoName',
-        message:'Please enter the name of your repo:',
+        name:'project',
+        message:'Please enter the project name:',
         //make sure something was entered
         validate: function(answer) {
             if (answer.length < 1) {
@@ -35,11 +36,57 @@ const questions = [
             }
             return true;
         }
+    },
+    // title of project
+    {
+        type: 'input',
+        name: 'function',
+        message: 'What does your project do?',
+        //make sure something was entered
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a title for your project:");
+            }
+        }
+    },
+    //motivation for project
+    {
+        type: 'input',
+        name: 'motiv',
+        message: 'What was your motivation for this project?'
+    },
+    //languages section
+    {
+        type: 'list',
+        name: 'languages',
+        Choices: ["html", "css", "js", "node", "python", 'c++', 'c.', 'java', 'c#', 'visual basic'],
+        message: 'What languages did you use on you project?'
+    },
+    //installation
+    {
+        type: 'input',
+        name: 'instruct',
+        message: 'How do you use your application?'
+    },
+    //licenses
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'Choose the license for your project',
+        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0']
     }
+    
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log();
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
