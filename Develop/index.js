f81114d65614d9cec89963a9abef25c5766e4989
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-09-18 16:51:41 -0600

    added some questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,38 @@ const util = require('util')
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
-    
+    //get users name
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?' 
+    },
+    //github username
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter your GitHub Username:',
+    //check to see if anything was entered
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("You must enter a GitHub Username.");
+        }
+        return true;
+    }
+    },
+    //get repos
+    {
+        type:'input',
+        name:'repoName',
+        message:'Please enter the name of your repo:',
+        //make sure something was entered
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter the name of the repo.");
+            }
+            return true;
+        }
+    }
 ];
 
 // TODO: Create a function to write README file
